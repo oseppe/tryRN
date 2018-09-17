beb26246bdb222777bd5db6be69058f57056a721
j montalbo <joseph@josephs-MacBook-Pro.local>
2018-09-17 12:42:24 +0800

    Use name param for naming the files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 const gulp = require('gulp');
 const template = require('gulp-template');
-const extReplace = require('gulp-ext-replace');
 const mkdirp = require('mkdirp');
 const yargs = require('yargs');
 const PluginError = require('plugin-error');
+const rename = require('gulp-rename');
 
 gulp.task('create:screen', () => {
-  const args = yargs.argv;
+  const { name } = yargs.argv;
 
   // Throw an error when name param is not passed
-  if (args.name === undefined) {
+  if (name === undefined) {
     throw new PluginError(
       'create:screen',
       'Screen Name missing. Provide one by using the format: create:screen --name <ScreenName>',
@@ -18,33 +18,45 @@ gulp.task('create:screen', () => {
 
   // Throw an error when ScreenName already exists
 
-  console.log('no errors');
+  // console.log('no errors');
 
-  // mkdirp('./App/Screens/Sample');
+  mkdirp(`./App/Screens/${name}`);
 
-  // gulp.src('./App/templates/componentJS.txt')
-  //   .pipe(template({
-  //     componentName: 'Sample',
-  //   }))
-  //   .pipe(extReplace('.js'))
-  //   .pipe(gulp.dest('App/Screens/Sample'));
+  gulp.src('./App/templates/componentJS.txt')
+    .pipe(template({
+      componentName: name,
+    }))
+    .pipe(rename({
+      basename: name,
+      extname: '.js',
+    }))
+    .pipe(gulp.dest(`./App/Screens/${name}`));
 
-  // gulp.src('./App/templates/componentJSTest.txt')
-  //   .pipe(extReplace('.test.js'))
-  //   .pipe(gulp.dest('App/Screens/Sample'));
+  gulp.src('./App/templates/componentJSTest.txt')
+    .pipe(rename({
+      basename: name,
+      extname: '.test.js',
+    }))
+    .pipe(gulp.dest(`./App/Screens/${name}`));
 
-  // gulp.src('./App/templates/index.txt')
-  //   .pipe(template({
-  //     componentName: 'Sample',
-  //   }))
-  //   .pipe(extReplace('.js'))
-  //   .pipe(gulp.dest('App/Screens/Sample'));
+  gulp.src('./App/templates/index.txt')
+    .pipe(template({
+      componentName: name,
+    }))
+    .pipe(rename({
+      extname: '.js',
+    }))
+    .pipe(gulp.dest(`./App/Screens/${name}`));
 
-  // gulp.src('./App/templates/styles.txt')
-  //   .pipe(extReplace('.js'))
-  //   .pipe(gulp.dest('App/Screens/Sample'));
+  gulp.src('./App/templates/styles.txt')
+    .pipe(rename({
+      extname: '.js',
+    }))
+    .pipe(gulp.dest(`./App/Screens/${name}`));
 
-  // gulp.src('./App/templates/api.txt')
-  //   .pipe(extReplace('.js'))
-  //   .pipe(gulp.dest('App/Screens/Sample'));
+  gulp.src('./App/templates/api.txt')
+    .pipe(rename({
+      extname: '.js',
+    }))
+    .pipe(gulp.dest(`./App/Screens/${name}`));
 });
